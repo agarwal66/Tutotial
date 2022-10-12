9edc8a0db9904a18c767dd3b5090b24f4f223151
prateek <agarwalprateek666@gmail.com>
2022-10-12 10:26:16 +0530

    python rep

---
+++ tutorial.py
@@ -0,0 +1,11 @@
+numbers = [2,4,3,7,6,9,4]
+numbers.append(0)
+print(numbers)
+numbers.insert(1,6)
+print(numbers)
+numbers.pop(2)
+print(numbers)
+del numbers[1:4]
+print(numbers)
+numbers.clear()
+print(numbers)
